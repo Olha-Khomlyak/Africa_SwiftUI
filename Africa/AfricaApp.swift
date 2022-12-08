9884fed558f55e219a0f003246d9e3f3897fdfe7
Olha Khomlyak <olga.khomlyak@gmail.com>
2022-12-08 15:45:32 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Olha Khomlyak on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
